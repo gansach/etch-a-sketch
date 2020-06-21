13da80dd68935546d59171a8648682271701519a
gansach <sachdevagandharv@gmail.com>
2020-06-21 18:09:34 +0530

    Improved css styling

--- sketch.js
+++ sketch.js
@@ -7,7 +7,7 @@ function createGrid(size) {
     for (let i = 0; i < gridSize ** 2; i++) {
         let box = document.createElement('div');
         box.style.width = `${100 / gridSize}%`;
-        //box.style.boxShadow = 'inset 0px 0px 0px 1px #000';
+        box.style.boxShadow = 'inset 0px 0px 0px 1px #000';
         box.className = 'box';
         box.dataset.hover = 0;
         sketchPad.appendChild(box);
@@ -21,13 +21,14 @@ sketchPad.addEventListener('mouseover', function(event) {
 
     if (box.className === 'box') {
         if (rainbowCheck.checked) {
-            shadeCheck.checked = false;
+            box.dataset.hover = 0;
             let randomColor = Math.floor(Math.random()*16777215).toString(16);
             box.style.background = `#${randomColor}`;
         } else if (shadeCheck.checked) {
             box.dataset.hover++;
             box.style.background = `rgba(0, 0, 0, ${box.dataset.hover / 10})`;           
         } else {
+            box.dataset.hover = 0;
             box.style.background = '#000';
         } 
     } 
@@ -35,6 +36,21 @@ sketchPad.addEventListener('mouseover', function(event) {
 
 function getGridSize() {
     let size = prompt('Enter size of grid: ', 16);
+    if (size === '0' || size === null) {
+        size = 16;
+    }
     createGrid(parseInt(size));
 }
+
+let rainbowCheck = document.getElementById('rainbowCheck');
+let shadeCheck = document.getElementById('shadeCheck');
+
+rainbowCheck.addEventListener('click', function() {
+    shadeCheck.checked = false;
+});
+
+shadeCheck.addEventListener('click', function() {
+    rainbowCheck.checked = false;
+});
+
 createGrid(2);
